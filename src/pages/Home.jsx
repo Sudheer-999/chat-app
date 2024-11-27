598d5d4c9ce627a44eeda18af6bd313de806ad2b
DESKTOP-55VPQEO\Dell <Sudheer.g@quantana.in>
2024-11-27 23:24:24 +0530

    pushing the project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+import '../assets/styles/Home.css'
+
+const Homepage = () => {
+    
+    return(
+        <div className="homepage">
+            <h1>Homepage</h1>
+        </div>
+    )
+}
+
+export default Homepage
